0cbff64212c824d860634c5110f780b2da5f46a5
abhinavtk <abhitk7310@gmail.com>
2024-04-15 20:43:42 +0530

    testing rust env

---
+++ target/debug/deps/flaxandteal_exc-1691139002598c62.d
@@ -0,0 +1,5 @@
+C:\Users\abhin\OneDrive\Documents\GitHub\flaxandteal-exercise\target\debug\deps\libflaxandteal_exc-1691139002598c62.rmeta: src\main.rs
+
+C:\Users\abhin\OneDrive\Documents\GitHub\flaxandteal-exercise\target\debug\deps\flaxandteal_exc-1691139002598c62.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/flaxandteal_exc-1f27b91d94a746ef.d
@@ -0,0 +1,5 @@
+C:\Users\abhin\OneDrive\Documents\GitHub\flaxandteal-exercise\target\debug\deps\libflaxandteal_exc-1f27b91d94a746ef.rmeta: src\main.rs
+
+C:\Users\abhin\OneDrive\Documents\GitHub\flaxandteal-exercise\target\debug\deps\flaxandteal_exc-1f27b91d94a746ef.d: src\main.rs
+
+src\main.rs:
