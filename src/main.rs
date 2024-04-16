ff1db79ba661adde61c5d3348c75e9cc9a1f7ed4
abhinavtk <abhitk7310@gmail.com>
2024-04-16 18:14:05 +0530

    Updated main file to take user input
    Now the main file supports taking input from user to select which program to run.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
-
-// Import the `rk4_ivp` module
 mod rk4_ivp;
 use rk4_ivp::rk4;
 
-// Import the `rk4_burgers` module
 mod rk4_burgers;
 use rk4_burgers::solve_burgers;
 
+use std::io;
 
 fn main() {
-    // Call the function from rk4
-    println!("\nSolving IVP Using RK4...");
-    rk4();
-    
+    println!("Choose an option:");
+    println!("1. Solve IVP using RK4");
+    println!("2. Solve Burger's equation using RK4");
 
-    println!("\nSolving Burger's equation using RK4...");
-    solve_burgers();
+    // Read user input
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
 
+    // Parse input to u32
+    let choice: u32 = match input.trim().parse() {
+        Ok(num) => num,         // Return num if input is valid
+        Err(_) => {
+            println!("Invalid input. Please enter 1 or 2.");    // Print error message if input is invalid
+            return;
+        }
+    };
+
+    // Match user input to choose function
+    match choice {
+        1 => {
+            println!("\nSolving IVP Using RK4 method...");
+            rk4();      // Call rk4 function
+        }
+        2 => {
+            println!("\nSolving Burger's equation using RK4 method...");
+            solve_burgers();        // Call solve_burgers function
+        }
+        _ => println!("Invalid option. Please choose 1 or 2."),   // Print error message if input is invalid
+    }
 }
